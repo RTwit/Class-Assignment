92e5e45f266f2859df4f03057b23790878a5fd77
RTwit <1450805810@qq.com>
2026-05-10 15:10:58 +0800

    5.1.下午更新代码

---
+++ Class Assignment/7.1.2多次求不同区间的数的和.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//
+//void sum(int begin, int end) {
+//	int i;
+//	int sum = 0;
+//	for (i = begin; i <= end; i++) {
+//		sum += i;
+//	}
+//	printf("%d到%d的和是%d\n", begin, end, sum);
+//}
+//
+//int main() {
+//	sum(1, 10);
+//	sum(20, 35);
+//	sum(45, 100);
+//
+//	return 0;
+//}
