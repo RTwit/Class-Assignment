bf330e23a1550788ff73f0ef101c15114151579d
RTwit <1450805810@qq.com>
2026-05-18 17:15:03 +0800

    未更改

--- Class Assignment/7.1.3-参数传递Test.c
+++ Class Assignment/7.1.3-参数传递Test.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 
+void swap(int a, int b)£»
 void cheer(int i) {
 	printf("cheer %d\n", i);
 }
 
 int main() {
-	cheer(2.4);
+	//cheer(2.4);
+	int a = 5, b = 6;
+	swap(a, b);
+	printf("a=%d b=%d\n", a, b);
 
 	return 0;
+}
+
+void swap(int a, int b) {
+	int t = a;
+	a = b;
+	b = t;
 }
